7a04474a40ccfbf69a0e958d85a2d8a1523fb043
luciebottin <lucie.bottin@efrei.net>
2022-06-27 17:19:34 +0200

    final commit

--- data.tf
+++ data.tf
@@ -2,13 +2,13 @@ data "azurerm_resource_group" "tp4" {
     name = "devops-TP2"
 }
 
-data "azurerm_virtual_network" "example-network" {
+data "azurerm_virtual_network" "net_tp4" {
     name = "example-network"
     resource_group_name = data.azurerm_resource_group.tp4.name
 } 
 
-data "azurerm_subnet" "internal" {
+data "azurerm_subnet" "sub_tp4" {
     name = "internal"
     resource_group_name   =    data.azurerm_resource_group.tp4.name 
-    virtual_network_name   =   data.azurerm_virtual_network.example-network.name 
+    virtual_network_name   =   data.azurerm_virtual_network.net_tp4.name 
 }
